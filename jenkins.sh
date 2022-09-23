822020b424e7ee9216fed50d5701f9e89276cb02
sauravmishra1234 <73914633+sauravmishra1234@users.noreply.github.com>
2022-09-23 01:20:29 -0700

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo apt install default-jdk -y
+sudo mkdir -p /usr/share/keyrings
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y
+sudo apt install jenkins -y
+sudo systemctl status jenkins --no-pager -l 
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
